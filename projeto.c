5c3eb2210af1da837ccfab32b66621015fee32ca
Gabriel Junges Baratto <gabrieljbaratto@gmail.com>
2021-08-25 19:13:06 -0300

    Configuração inicial do Timer0

--- projeto.c
+++ projeto.c
@@ -1,6 +1,18 @@
 #include <msp430.h> 
 #include <stdio.h>
 
+void ini_P1_P2(void);
+void ini_uCon(void);
+void ini_Timer0(void);
+
+
+void main(void)
+{
+    ini_P1_P2();
+    ini_uCon();
+    ini_Timer0();
+}
+
 void ini_P1_P2(void){
     // PORTA 1
     P1DIR = ~(BIT2 + BIT4 + BIT6 + BIT7); // = BIT0 + BIT1 + BIT3 + BIT5
@@ -27,7 +39,7 @@ void ini_P1_P2(void){
 
 void ini_uCon(void){
     //Configuração do sistema de clock.
-    WDTCTL = WDTPW | WDTHOLD;
+    WDTCTL = WDTPW | WDTHOLD; // parando watchdog timer
 
     DCOCTL = CALDCO_1MHZ;
     BCSCTL1 = CALBC1_1MHZ;
@@ -37,9 +49,43 @@ void ini_uCon(void){
     __enable_interrupt();
 }
 
-void main(void) {
-    ini_P1_P2();
-    ini_uCon();
+void ini_Timer0(void){
+    /* Inicializacao do TImer 0 para o debouncer das chaves e do encoder, e disparo
+     * de conversão para o ADC10 (tempo de amostragem)
+     *
+     * CONTADOR:
+     *      - Clock: ACLK ~ 32768Hz
+     *          - Fdiv: 1
+     *      - Modo contagem: UP (inicialmente ficara no modo PARADO)
+     *      - Int. do contador: desabilitada
+     *
+     * MODULO 0: (resposnável pelo deb_encoder)
+     *      - Funcao: comparacao (nativa)
+     *      - Int.: Habilitada
+     *      Tempo debouncer encoder deb_enc_time = 3 ms
+     *      - Valor para TA0CCR0: 32768 * 3e-3 - 1 = 97
+     *
+     * MODULO 1: (debounce das chaves)
+     *      - Funcao: comparacao (nativa)
+     *      - Int.: Habilitada
+     *      Tempo debouncer chaves deb_ch_time = 12 ms
+     *      - Valor para TA0CCR1: 32768 * 12e-3 - 1  = 392
+     *
+     * MODULO 2: (disparo de conversão)
+     *      - Funcao: comparacao (nativa)
+     *      - Int.: Habilitada
+     *      Tempo amostragem t_samps = 150 ms
+     *      - Valor para TA0CCR2: 4914
+     */
+// Clock base Timer A0:
+    TA0CTL = TASSEL0;
+// Modulo 0
+    TA0CCTL0 = CCIE;
+    TA0CCR0 = 97;
+// Modulo 1
+    TA0CCTL1 = CCIE;
+    TA0CCR1 = 392;
+// Modulo 2
+    TA0CCTL2 = CCIE;
+    TA0CCR2 = 4914;
 }
-
-
